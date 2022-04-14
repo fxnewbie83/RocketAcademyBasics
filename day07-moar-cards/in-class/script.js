b909fc5c58517573238566c9faa710ed40aa2661
fxnewbie83 <100143764+fxnewbie83@users.noreply.github.com>
2022-04-14 21:34:26 +0800

    Day 7 In Class

--- day07-moar-cards/in-class/script.js
+++ day07-moar-cards/in-class/script.js
@@ -1,3 +1,192 @@
+//Build an app using the pre-class deck functions such that when the user clicks Submit,
+//draw a single card from a shuffled deck and show it to them.
+
+// Helper desk for new deck
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  // Loop over the suits array
+  for (var i = 0; i < suits.length; i += 1) {
+    // Store the current suit in a variable
+    var currentSuit = suits[i];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    for (var rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+    }
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+var moarCardsSingleCardMain = function (input) {
+  var cardDeck = makeDeck();
+  var shuffledDeck = shuffleCards(cardDeck);
+  console.log(cardDeck);
+
+  var userCard = {
+    name: shuffledDeck[0].name,
+    rank: shuffledDeck[0].rank,
+    suit: shuffledDeck[0].suit
+  };
+  console.log(userCard);
+
+  var myOutputValue = `${userCard.name} ${userCard.rank} ${userCard.suit}`;
+  return myOutputValue;
+};
+
+// ========================================================================================================================================
+var cardDeck = makeDeck();
+var shuffledDeck = shuffleCards(cardDeck);
+
+var moarCardsLowCardMain = function (input) {
+  // Draw 2 cards from the top of the deck
+  var computerCard = shuffledDeck.pop();
+  var playerCard = shuffledDeck.pop();
+
+  // Construct an output string to communicate which cards were drawn
+  var myOutputValue =
+    "Computer had " +
+    computerCard.name +
+    " of " +
+    computerCard.suit +
+    ". Player had " +
+    playerCard.name +
+    " of " +
+    playerCard.suit +
+    ". ";
+
+  // Compare computer and player cards by rank attribute
+  // If computer card rank is greater than player card rank, computer wins
+  if (computerCard.rank < playerCard.rank) {
+    // Add conditional-dependent text to the output string
+    myOutputValue = myOutputValue + "Computer wins.";
+    // Else if computer card rank is less than player card rank, player wins
+  } else if (computerCard.rank > playerCard.rank) {
+    myOutputValue = myOutputValue + "Player wins!";
+    // Otherwise (i.e. ranks are equal), it's a tie
+  } else {
+    myOutputValue = myOutputValue + "It's a tie.";
+  }
+
+  // Return the fully-constructed output string
+  return myOutputValue;
+};
+
+// ========================================================================================================================================
+
+// The player can input how many cards they want.
+// The lowest of the player's cards is compared against the computer's 1 card.
+// Show the player all the cards that they drew.
+// Rules from above exercises continue to apply.
+
+var moarCardsLowCardHandsMain = function (input) {
+  var computerCard = shuffledDeck.pop();
+  var playerCardOne = shuffledDeck.pop();
+  var playerCardTwo = shuffledDeck.pop();
+  var playerCardThree = shuffledDeck.pop();
+
+  var playerCard = []; //[playerCardOne, playerCardTwo, playerCardThree];
+  for (var i = 1; i <= input; i++) {
+    var onePlayerCard = shuffledDeck.pop();
+    playerCard.push(onePlayerCard);
+  }
+  //mininum
+  var minCardRank = 100;
+  for (var i = 1; i < playerCard.length; i++) {
+    if (playerCard[i].rank < minRank) {
+      minCardRank = playerCard[i].rank;
+      minCardName = playerCard[i].name;
+      minCardSuit = playerCard[i].suit;
+    }
+  }
+
+  // Construct an output string to communicate which cards were drawn
+  var myOutputValue =
+    "Computer had " +
+    computerCard.name +
+    " of " +
+    computerCard.suit +
+    ". Player had " +
+    playerCard.name +
+    " of " +
+    playerCard.suit +
+    ". ";
+
+  // Compare computer and player cards by rank attribute
+  // If computer card rank is greater than player card rank, computer wins
+  if (computerCard.rank < playerCard.rank) {
+    // Add conditional-dependent text to the output string
+    myOutputValue = myOutputValue + "Computer wins.";
+    // Else if computer card rank is less than player card rank, player wins
+  } else if (computerCard.rank > playerCard.rank) {
+    myOutputValue = myOutputValue + "Player wins!";
+    // Otherwise (i.e. ranks are equal), it's a tie
+  } else {
+    myOutputValue = myOutputValue + "It's a tie.";
+  }
+
+  var myOutputValue = "hello world";
+  return myOutputValue;
+};
+
+// ========================================================================================================================================
+
+/*
 var moarCardsSingleCardMain = function (input) {
   // Complete the Base: Moar Cards Display Single Card exercise below with moarCardsSingleCardMain as the main function.
   var myOutputValue = 'hello world';
@@ -99,3 +288,4 @@ var chatBotFortuneMain = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
+*/
